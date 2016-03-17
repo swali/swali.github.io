66c8bc32ffa3f2581af1312695b4d5030a3dc7aa
Sandeep Wali <sandeep1986@msn.com>
2016-03-17 02:47:32 -0700

    rewrote slider so as to handle loopback better

--- js/slider.js
+++ js/slider.js
@@ -1,18 +1,36 @@
 function Slider () {
-	var imageCount = 0,
-		total = 5,
-		sliderInterval;
+	var images = [],
+		imageCount = 5,
+		sliderInterval = null,
+		sliderImagesContainer = document.querySelector("#sliderImagesContainer");
 
-	//run this everytime arrow is clicked
-	function slide(x) {
-		imageCount = imageCount + x;
-		if (imageCount < 0) {
-			imageCount = total - 1;
-		} else if (imageCount >= total) {
-			imageCount = 0;
+	// run this everytime arrow is clicked
+	function slide(direction) {
+		if (direction > 0) {
+			var temp = images[0];
+			temp.style.zIndex = -1;
+			temp.style.left = ((imageCount - 2) * 100) + "vw";
+
+			for (var i = 0; i < imageCount - 1; i++) {
+				images[i] = images[i + 1];
+				images[i].style.removeProperty('z-index');
+				images[i].style.left = ((i - 1) * 100) + "vw";
+			}
+
+			images[imageCount - 1] = temp;
+		} else {
+			var temp = images[imageCount - 1];
+			temp.style.zIndex = -1;
+			temp.style.left = "-100vw";
+
+			for (var i = imageCount - 1; i > 0; i--) {
+				images[i] = images[i - 1];
+				images[i].style.removeProperty('z-index');
+				images[i].style.left = ((i - 1) * 100) + "vw";
+			}
+
+			images[0] = temp;
 		}
-
-		document.getElementById("sliderImagesContainer").style.left = "-" + (100 * (imageCount)) + "vw";
 	}
 
 	function initialize() {
@@ -41,15 +59,31 @@ function Slider () {
 	}
 
 	function createSliderImageContainers () {
-		var imageElement,
-			sliderImagesContainer = document.querySelector("#sliderImagesContainer");
-
-		for (var i = 0; i < total; i++) {
-			imageElement = document.createElement("div");
-			imageElement.className = "image";
-			imageElement.style.backgroundImage = "url('images/slider/" + i + ".jpg')";
-			sliderImagesContainer.appendChild(imageElement);
+		for (var i = -1; i < imageCount - 1; i++) {
+			images.push(addImage(i));
 		}
+	}
+
+	function addImage(index) {
+		var imageElement = document.createElement("div");
+		imageElement.className = "image";
+		imageElement.style.backgroundImage = "url('images/slider/" + fixIndex(index) + ".jpg')";
+		imageElement.style.position = "absolute";
+		imageElement.style.left = (100 * index) + "vw";
+		
+		sliderImagesContainer.appendChild(imageElement);
+
+		return imageElement;
+	}
+
+	function fixIndex(index) {
+		if (index < 0) {
+			index += imageCount;
+		} else if (index >= imageCount) {
+			index -= imageCount;
+		}
+
+		return index;
 	}
 
 	return {
