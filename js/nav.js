34ce6065f0e7bdd715466ac05ea3c9f8aa610e20
Rashna Razdan <rashnarazdan@gmail.com>
2016-03-20 01:16:57 -0400

    people section content and links enhancement for nav

--- js/nav.js
+++ js/nav.js
@@ -7,10 +7,58 @@ function Nav () {
 	    }
 	}
 
+	function underlineLink() {
+		var prevActive = document.querySelector(".activeLink");
+		if (prevActive) prevActive.classList.remove("activeLink");
+		this.classList.add("activeLink");
+	}
+
+	function controlCurrentNav() {
+		var sections = document.querySelectorAll(".sectionContent"),
+			nav = document.querySelector("#nav"),
+				windowPos = window.pageYOffset,
+				navHeight = nav.offsetHeight,
+				//subtract extra 70px overflow padding-top and add 20px top/bottom section padding
+				sliderContainerHeight = document.querySelector("#sliderContainer").offsetHeight, 
+				aboutUsHeight = document.querySelector("#story").offsetHeight - 70 + 40,
+			    proposalHeight = document.querySelector("#proposal").offsetHeight - 70 + 40,
+			    eventsHeight = document.querySelector("#events").offsetHeight - 70 + 40,
+			    firstSection = sliderContainerHeight,
+			    secondSection = aboutUsHeight + sliderContainerHeight,
+			    thirdSection = proposalHeight + aboutUsHeight + sliderContainerHeight,
+			    fourthSection = eventsHeight + proposalHeight + aboutUsHeight + sliderContainerHeight;
+		    
+		    if (windowPos < firstSection - navHeight) {
+		    	var prevActive = document.querySelector(".activeLink");
+				if (prevActive) prevActive.classList.remove("activeLink");
+				return;
+		    }
+			if (windowPos > (fourthSection - navHeight)) {
+				underlineLink.call(document.querySelector("a[href='#people']"));
+			}
+			else if (windowPos > (thirdSection - navHeight)) {
+				underlineLink.call(document.querySelector("a[href='#events']"));
+			}
+			else if (windowPos > secondSection - navHeight) {
+				underlineLink.call(document.querySelector("a[href='#proposal']"));
+			}
+				else if (windowPos > (firstSection - navHeight)) {
+				underlineLink.call(document.querySelector("a[href='#story']"));
+			}
+	}
+
 	function initialize () {
 		window.addEventListener("scroll", function() {
 			controlNavigationShadow();
+			controlCurrentNav.call(this);
 		}, false);
+		var links = document.querySelectorAll("#nav a");
+		for(var i = 0; i < links.length; i++) {
+			var link = links[i];
+			link.addEventListener("click", function() {
+				underlineLink.call(this);
+			}, false);
+		}
 	}
 
 	return {
