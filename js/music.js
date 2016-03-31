a0c88fb0fd00d994b4fadc8bdc31abeb8af33f4f
Rashna Razdan <rashnarazdan@gmail.com>
2016-03-31 14:18:04 -0400

    improved arrow html and css and position of audio

--- js/music.js
+++ js/music.js
@@ -1,18 +1,21 @@
 function MusicPlayer() {
 	
-	var audioControl = document.querySelector("#music audio");
+	var audioControl = document.querySelector("#music audio"),
+		audioContainer = document.getElementById("music");
+	
 	audioControl.volume = 0.4;
 
 	function initialize() {
-		var audioContainer = document.getElementById("music");
 		audioContainer.addEventListener("click", toggleMusic);
 	}
 
 	function toggleMusic() {
 		if (!audioControl.paused) {
 			audioControl.pause();
+			audioContainer.style.backgroundColor = "rgba(255, 255, 255, 0.3)";
 		} else {
 			audioControl.play();
+			audioContainer.style.backgroundColor = "rgba(255, 255, 255, 0.6)";
 		}
 	}
 
